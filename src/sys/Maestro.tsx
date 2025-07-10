c70a7c82a42fdf7fdf71abb176516e170d26c01a
bitbyte08 <me@bitworkspace.kr>
2025-07-11 00:30:11 +0900

    feat :: 테스트 드레그 로직 구현

--- src/sys/Maestro.tsx
+++ src/sys/Maestro.tsx
@@ -1,11 +1,54 @@
-import React from 'react';
-
+import React, {useState, useCallback, useRef} from 'react';
+import { useDrag } from 'react-use-drag';
+import '@/etc/config.css';
 const Maestro:React.FC = () => {
   return (
     <>
       Maestro
+      <App />
     </>
   )
 }
 
+
+const App = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const positionOffsetRef = useRef({ x: 0, y: 0 })
+
+  const onRelativePositionChange = useCallback((x: number, y: number) => {
+    const prevOffset = positionOffsetRef.current
+
+    setPosition((position) => ({
+      x: position.x + (x - prevOffset.x),
+      y: position.y + (y - prevOffset.y),
+    }))
+
+    positionOffsetRef.current = { x, y } // 👈 setState 대신 ref에 저장
+  }, [])
+  const onStart = useCallback(() => {
+    console.log('Dragging has started')
+  }, [])
+  const onEnd = useCallback((x, y) => {
+    setPosition((position) => ({
+      x: position.x + x,
+      y: position.y + y,
+    }))
+  }, [])
+  const { elementProps, isMoving } = useDrag({
+    onRelativePositionChange,
+    onStart,
+    onEnd,
+  })
+  return (
+    <button
+      className="draggable"
+      style={{
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }}
+      {...elementProps}
+    >
+      {isMoving ? '🚶' : '🧍'}
+    </button>
+  )
+}
 export default Maestro
