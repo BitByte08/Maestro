61ffbef14ee435cbf66ec39a98499292f653f050
bitbyte08 <me@bitworkspace.kr>
2025-07-14 16:15:49 +0900

    fix :: 로직 수정

--- src/usr/layouts/Appication.tsx
+++ src/usr/layouts/Appication.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useRef, useState} from "react";
+import React, {useCallback, useState} from "react";
 import {useDrag} from "react-use-drag";
 
 type PositionType = {
@@ -6,38 +6,28 @@ type PositionType = {
   y: number;
 }
 const Application: React.FC = () => {
-  const [position, setPosition] = useState<PositionType>({ x: 0, y: 0 })
-  const positionOffsetRef = useRef<PositionType>({ x: 0, y: 0 })
-
-  const onRelativePositionChange = useCallback((x: number, y: number) => {
-    const prevOffset: PositionType = positionOffsetRef.current
-
-    setPosition((position) => ({
-      x: position.x + (x - prevOffset.x),
-      y: position.y + (y - prevOffset.y),
-    }))
-
-    positionOffsetRef.current = { x, y } // 👈 setState 대신 ref에 저장
-  }, [])
-  const onStart = useCallback(() => {
-    console.log('Dragging has started')
-  }, [])
+  const [position, setPosition] = useState<PositionType>({ x: 0, y: 0 });
+  const [positionOffset, setPositionOffset] = useState<PositionType>({ x: 0, y: 0 });
+  const onRelativePositionChange = useCallback((x: number, y: number) => setPositionOffset({ x, y }), []);
   const onEnd = useCallback((x: number, y: number) => {
     setPosition((position) => ({
       x: position.x + x,
       y: position.y + y,
-    }))
-  }, [])
+    }));
+    setPositionOffset({ x: 0, y: 0 });
+  }, []);
   const { elementProps, isMoving } = useDrag({
     onRelativePositionChange,
-    onStart,
     onEnd,
-  })
+  });
   return (
     <button
       className="draggable"
       style={{
-        transform: `translate(${position.x}px, ${position.y}px)`,
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        transform: `translate(${position.x + positionOffset.x}px, ${position.y + positionOffset.y}px)`,
       }}
       {...elementProps}
     >
