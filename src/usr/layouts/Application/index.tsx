47089d9ac23f81e2ab15ea1e6da78325793d19a4
bitbyte08 <me@bitworkspace.kr>
2025-07-15 11:00:42 +0900

    feat :: 오른쪽 사이즈 조절 구현

--- src/usr/layouts/Application/index.tsx
+++ src/usr/layouts/Application/index.tsx
@@ -1,20 +1,32 @@
-import React, {type PointerEvent, useCallback, useState} from "react";
-import {useDrag} from "react-use-drag";
-import {ContantContainer, HeaderContainer} from './styles.tsx';
+import React, { useCallback, useState } from "react";
+import { useDrag } from "react-use-drag";
+import { ContantContainer, HeaderContainer } from './styles.tsx';
 
 type PositionType = {
   x: number;
   y: number;
 }
-interface HeaderProps {
-  elementProps: {
-    onPointerDown: (event: PointerEvent<HTMLElement>) => void;
-    onPointerUp: ((event: PointerEvent<HTMLElement>) => void) | undefined;
-    onPointerMove: ((event: PointerEvent<HTMLElement>) => void) | undefined;
-    onPointerCancel: ((event: PointerEvent<HTMLElement>) => void) | undefined;
-  };
+type SizeType = {
+  width: number;
+  height: number;
 }
-const Header: React.FC<HeaderProps> = ({elementProps}:HeaderProps) => {
+interface HeaderProps {
+  setPosition: React.Dispatch<React.SetStateAction<PositionType>>,
+  setPositionOffset: React.Dispatch<React.SetStateAction<PositionType>>
+}
+const Header: React.FC<HeaderProps> = ({setPosition, setPositionOffset}: HeaderProps) => {
+  const onRelativePositionChange = useCallback((x: number, y: number) => setPositionOffset({ x, y }), []);
+  const onEnd = useCallback((x: number, y: number) => {
+    setPosition((position) => ({
+      x: position.x + x,
+      y: position.y + y,
+    }));
+    setPositionOffset({ x: 0, y: 0 });
+  }, []);
+  const { elementProps } = useDrag({
+    onRelativePositionChange,
+    onEnd,
+  });
   return (
     <HeaderContainer className="draggable" {...elementProps}>
       Header
@@ -24,18 +36,22 @@ const Header: React.FC<HeaderProps> = ({elementProps}:HeaderProps) => {
 const Application: React.FC = () => {
   const [position, setPosition] = useState<PositionType>({ x: 0, y: 0 });
   const [positionOffset, setPositionOffset] = useState<PositionType>({ x: 0, y: 0 });
-  const onRelativePositionChange = useCallback((x: number, y: number) => setPositionOffset({ x, y }), []);
+  const [size, setSize] = useState<SizeType>({width: 200, height: 200});
+  const [sizeOffset, setSizeOffset] = useState<SizeType>({width: 0, height: 0});
+
+  const onRelativePositionChange = useCallback((x: number, y: number) => setSizeOffset({ width: x, height: y }), []);
   const onEnd = useCallback((x: number, y: number) => {
-    setPosition((position) => ({
-      x: position.x + x,
-      y: position.y + y,
-    }));
-    setPositionOffset({ x: 0, y: 0 });
+    setSize((size)=>({
+      width: size.width + x,
+      height: size.height + y,
+    }))
+    setSizeOffset({width: 0, height: 0});
   }, []);
-  const { elementProps, isMoving } = useDrag({
+  const { elementProps } = useDrag({
     onRelativePositionChange,
     onEnd,
   });
+
   return (
     <section
       style={{
@@ -43,18 +59,21 @@ const Application: React.FC = () => {
         left: 0,
         top: 0,
         transform: `translate(${position.x + positionOffset.x}px, ${position.y + positionOffset.y}px)`,
-        height: '100px',
-        width: '100px',
+        height: `${size.height + sizeOffset.height}px`,
+        width: `${size.width + sizeOffset.width}px`,
       }}
     >
-      <Header elementProps={elementProps} />
+      <Header setPosition={setPosition} setPositionOffset={setPositionOffset} />
       <ContantContainer>
         <button
           className="draggable"
-
-
+          style={{
+            position: 'absolute',
+            right: 0,
+          }}
+          {...elementProps}
         >
-          {isMoving ? '🚶' : '🧍'}
+          {'🧍'}
         </button>
       </ContantContainer>
     </section>
