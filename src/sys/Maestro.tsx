c6263235f736a6eed6f4c6abcae5c1a379208207
bitbyte08 <me@bitworkspace.kr>
2025-10-13 17:53:01 +0900

    feat :: 작업표시줄 추가

--- src/sys/Maestro.tsx
+++ src/sys/Maestro.tsx
@@ -1,8 +1,16 @@
 import React, { useEffect } from 'react';
+import styled from "styled-components";
 import '@/etc/config.css';
 import Podium from "@/sys/layouts/Podium";
 import { useTaskStore } from "@/sys/store/taskStore.ts";
 import Quickly from "@/usr/bin/Quickly.ts";
+
+const Display = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
 
 const Maestro: React.FC = () => {
   const addTask = useTaskStore(state => state.addTask);
@@ -10,9 +18,10 @@ const Maestro: React.FC = () => {
     addTask(Quickly);
   })
   return (
-    <>
+    <Display>
       <Podium />
-    </>
+      <div>dfaedda</div>
+    </Display>
   )
 }
 
--- src/sys/layouts/Podium/index.tsx
+++ src/sys/layouts/Podium/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import * as Style from './styles.tsx';
 import {useTaskStore} from "@/sys/store/taskStore.ts";
 import Application from "@/usr/layouts/Application";
 
 const Podium: React.FC = () => {
   const tasks = useTaskStore(state => state.tasks);
   return (
-    <>
+    <Style.PodiumContainer>
       {tasks && tasks.map((task, index) => {
         const Component: React.FC = task.component;
         return (
@@ -14,7 +15,7 @@ const Podium: React.FC = () => {
           </Application>
         )
       })}
-    </>
+    </Style.PodiumContainer>
   )
 }
 
