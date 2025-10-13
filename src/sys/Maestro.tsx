fab95d28a0a3789d08bc2f5410327983e07705cb
bitbyte08 <me@bitworkspace.kr>
2025-10-13 17:38:57 +0900

    feat :: Quickly 초기 실행앱 추가

--- src/sys/Maestro.tsx
+++ src/sys/Maestro.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect } from 'react';
 import '@/etc/config.css';
 import Podium from "@/sys/layouts/Podium";
-import TopBar from "@/usr/bin/TopBar";
 import { useTaskStore } from "@/sys/store/taskStore.ts";
-import Terminal from "@/usr/bin/Terminal";
+import Quickly from "@/usr/bin/Quickly.ts";
 
 const Maestro: React.FC = () => {
   const addTask = useTaskStore(state => state.addTask);
   useEffect(() => {
-    addTask(TopBar);
-    addTask(Terminal)
+    addTask(Quickly);
   })
   return (
     <>
