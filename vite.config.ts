148f84ed2a57c9dcf72a1250426fd33c2b15df7d
bitbyte08 <me@bitworkspace.kr>
2025-11-23 09:14:45 +0900

    feat :: reactCompiler 적용

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [['babel-plugin-react-compiler', {}]],
+    },
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),  // <-- '@'를 'src'로 매핑
